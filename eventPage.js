09eae79dc56c4386bb82054dc449a23866fa6a28
Sanket Chauhan <sanke93@gmail.com>
2014-01-12 05:59:38 -0500

    successfully downloads ics files, but crashes upon opening gcal

--- eventPage.js
+++ eventPage.js
@@ -2,26 +2,27 @@ chrome.browserAction.onClicked.addListener(function() {
 	
 	//check if preference is defined through options
 	console.log(localStorage.preference);
-
-
-	if(localStorage.preference == "gCal"){
-		chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
 		     activeTabUrl = arrayOfTabs[0].url; // gets the URL
 		     activeTabTitle = arrayOfTabs[0].title;
 		     //alert(activeTabUrl);
 	  	});
+
+	if(localStorage.preference == "gCal"){
 	  	//alert(activeTabUrl);
 	    var href = "https://www.google.com/calendar/render?" + "action=TEMPLATE&text=Reminder "+ activeTabTitle + "&location=" + activeTabUrl + "&details="+ activeTabUrl+"&trp%3B=true&gsessionid=OK&output=xml";
-	    //sample google cal url
-	 	//https://www.google.com/calendar/render?action=TEMPLATE&text=ajsdasjhd&details=asd&trp%3B=true&gsessionid=OK&output=xml
-	  	window.open(href, 'Reminder', 'width=700,height=700');
+		window.open(href, 'Reminder', 'width=700,height=700');
 	}
 	else{
-		var eventReminder = "hi";
+		var eventFileStart = "BEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VEVENT\n";
+		var eventFileCustom = "SUMMARY:Reminder for"+activeTabTitle+"\nDESCRIPTION:Visit "+activeTabUrl+ "\nLOCATION:"+activeTabUrl+"\n";
+		var eventFileEnd = "END:VEVENT\nEND:VCALENDAR"
+		var eventReminder = eventFileStart + eventFileCustom + eventFileEnd;
 		var blob = new Blob([eventReminder], {type: "text/plain;charset=utf-8"});
 		saveAs(blob, "reminder.ics");
 	}
 });
+
 
 
 // Called when the user clicks on the browser action.
