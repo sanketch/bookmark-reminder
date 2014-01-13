cec9b1a474b8ace678d241608b06e3448de06f1e
Sanket Chauhan <sanke93@gmail.com>
2014-01-12 19:07:14 -0500

    fixed the declaration that prevented the function be run the first time + updated options.js to show current option

--- eventPage.js
+++ eventPage.js
@@ -1,5 +1,8 @@
 chrome.browserAction.onClicked.addListener(function() {
-	
+
+	var activeTabUrl;
+	var activeTabTitle
+
 	//check if preference is defined through options
 	if(localStorage.preference == undefined){
 		localStorage.preference = "gCal";
@@ -9,13 +12,13 @@ chrome.browserAction.onClicked.addListener(function() {
 	chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
 		    activeTabUrl = arrayOfTabs[0].url; // gets the URL
 		    activeTabTitle = arrayOfTabs[0].title;
-		     //alert(activeTabUrl);
+		    //alert(activeTabUrl);
 	  	});
 
 	if(localStorage.preference == "gCal"){
-	  	//alert(activeTabUrl);
 	    var href = "https://www.google.com/calendar/render?" + "action=TEMPLATE&text=Reminder "+ activeTabTitle + "&location=" + activeTabUrl + "&details="+ activeTabUrl+"&trp%3B=true&gsessionid=OK&output=xml";
 		window.open(href, 'Reminder', 'width=700,height=700');
+		//alert("gcal");
 	}
 	else{
 		var eventFileStart = "BEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VEVENT\n";
@@ -24,6 +27,7 @@ chrome.browserAction.onClicked.addListener(function() {
 		var eventReminder = eventFileStart + eventFileCustom + eventFileEnd;
 		var blob = new Blob([eventReminder], {type: "text/plain;charset=utf-8"});
 		saveAs(blob, "reminder.ics");
+		//alert("ical");
 	}
 });
 
--- options.js
+++ options.js
@@ -1,10 +1,8 @@
-//localStorage["preference"] = "gCal";
-
 function clickHandler(e) {
   	var select = document.getElementById("preference");
 	preference = select.children[select.selectedIndex].value;
 	localStorage["preference"] = preference;
-	//
+	document.getElementById("current").textContent = localStorage.preference;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
